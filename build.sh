2f9904345acc65c4abd0c11e2e752ca154173aad
Nagendra Kathi <157376864+nagendrakathi@users.noreply.github.com>
2025-04-17 17:04:38 +0000

    Add files

--- build.sh
+++ build.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+# Exit on error
+set -o errexit
+
+# Create and activate virtual environment
+python -m venv venv
+source venv/bin/activate
+
+# Install dependencies
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Only after installing dependencies, run database migrations
 python -c "from app import create_app, db; app = create_app(); app.app_context().push(); db.create_all()"
